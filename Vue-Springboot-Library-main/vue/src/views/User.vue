df871d3791521a5d2d4b44db26b3d07f7ffe031d
yeyue <1923780217@qq.com>
2025-12-16 22:16:36 +0800

    修复--user.vue右边空白显示

--- Vue-Springboot-Library-main/vue/src/views/User.vue
+++ Vue-Springboot-Library-main/vue/src/views/User.vue
@@ -36,8 +36,7 @@
       </el-popconfirm>
     </div>
 <!-- 数据字段-->
-    <div style="width: 1000px; overflow-x: auto; overflow-y: auto; height: calc(100vh - 230px); margin-left: 20px; margin-right: 20px;">
-    <el-table :data="tableData" stripe border="true"  @selection-change="handleSelectionChange" style="width: 1000px;">
+    <el-table :data="tableData" stripe border="true"  @selection-change="handleSelectionChange" style="width: 100%;">
       <el-table-column v-if="user.role ==1 "
                        type="selection"
                        width="55">
@@ -59,7 +58,6 @@
         </template>
       </el-table-column>
     </el-table>
-    </div>
 <!--    分页-->
     <div style="margin: 10px 0">
       <el-pagination
